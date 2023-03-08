ed1f21ecb086d202d90a03438bf296b102d246ca
Ivan Shalygin <unicorn@tut.by>
2023-03-08 11:58:44 +0300

    Update style collapse in html

---
+++ portfolio/static/portfolio/my.js
@@ -0,0 +1,25 @@
+//var $myGroup = $('#myGroup');
+//$myGroup.on('show.bs.collapse','.collapse', function() {
+//    $myGroup.find('.collapse').collapse('hide');
+//});
+//
+//const myCollapsible = document.getElementById('collapseExample')
+//const myCollapsible2 = document.getElementById('collapseExample2')
+//const myCollapsible3 = document.getElementById('collapseExample3')
+//
+//myCollapsible.addEventListener('show.bs.collapse', event => {
+//  myCollapsible2.collapse('hide');
+//  myCollapsible3.collapse('hide');
+//})
+//myCollapsible2.addEventListener('show.bs.collapse', event => {
+//  myCollapsible.collapse('hide');
+//  myCollapsible3.collapse('hide');
+//})
+//myCollapsible3.addEventListener('show.bs.collapse', event => {
+//  myCollapsible.collapse('hide');
+//  myCollapsible2.collapse('hide');
+//})
+//
+//jQuery('button').click( function(e) {
+//    jQuery('.collapse').collapse('hide');
+//});
