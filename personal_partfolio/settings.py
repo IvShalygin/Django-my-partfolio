476235e140f138b0f6378835e3aa27596f366629
Janek_Shalygin <61755149+IvShalygin@users.noreply.github.com>
2024-09-16 20:21:53 +0200

    Last update. For deploy

--- personal_partfolio/settings.py
+++ personal_partfolio/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os.path
 from django.utils.translation import gettext_lazy as _
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from django.contrib.messages import constants as messages
 
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = 'django-insecure-pgumv&0u$czzzs-(7*187_gw^72c@@^m@tu5q9rb63o+!f2ck_'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
